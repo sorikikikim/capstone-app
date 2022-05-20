7e78534fbf71066249ccd60d957b6f2d9b6b672c
sorikikikim <sorikim98@naver.com>
2022-05-20 13:25:46 +0900

    signup user - authentication

--- pages/home.js
+++ pages/home.js
@@ -3,32 +3,6 @@ import React from 'react';
 const Home = (props) => {
   return (
     <div>
-      
-    {/* <div id="login">
-        <button type="button" id='homeBtn' >로그인</button>
-        <button type="button" id='sellBtn' >회원가입</button>
-    </div>
-    <div id="header">
-        <p id="title"><img src="images/mainLogo.png"></img>&nbsp;한성마켓</p>
-    </div>
-    <div id="nav">
-        <div id="menu">
-            <button type="button"><img src="images/menubar.png" width="20" height="20" /></button>
-            <button type="button">홈</button>
-            <button type="button">판매등록</button>
-            <button type="button">상품검색</button>
-            <button type="button">문의하기</button> 
-        </div>
-        <div id="user">
-            <button type="button"><img src="images/user.png" width="20" height="20" /></button>
-            <button type="button"><img src="images/cart.png" width="20" height="20" /></button>
-            <button type="button"><img src="images/msg.png" width="20" height="20" /></button>
-            <form action="uri" method="post"> 
-                <input type="text" id='searchForm' size='20' name = "searchWord" placeholder="검색어를 입력하세요"></input> 
-                <button type="submit"><img src="images/search.png" width="20" height="20" /></button>
-            </form>
-        </div>
-    </div> */}
     <div id="banner">
         <img src="bannerImg.png" width="280" height="280"/> 
         <p><span id="bannerTitle">한성대학교만의 마켓 <br />
